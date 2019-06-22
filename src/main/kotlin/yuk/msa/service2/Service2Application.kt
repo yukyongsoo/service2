d9cdd2e2aa0592c453fc107e8ff62f1a14a843d4
yongsoo <yozx9079@naver.com>
2019-06-23 02:20:18 +0900

    Initial commit

---
+++ src/main/kotlin/yuk/msa/service2/Service2Application.kt
@@ -0,0 +1,11 @@
+package yuk.msa.service2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Service2Application
+
+fun main(args: Array<String>) {
+    runApplication<Service2Application>(*args)
+}
---
+++ src/test/kotlin/yuk/msa/service2/Service2ApplicationTests.kt
@@ -0,0 +1,16 @@
+package yuk.msa.service2
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Service2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
